0439d1e303867315edd25cc57b2c13c8d58716b1
ttmone <tomone.ameno@gmail.com>
2019-12-13 18:12:59 +0900

    一応、商品が表示できるようになった

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ import requests
 import base64
 import json
 import re
+import time
 
 dotenv.load_dotenv()
 
 app = Flask(__name__)
 
 
-def get_str(image):
-    context = base64.b64encode(image.read()).decode()   # decode()する必要ある？
+def request_to_gcp(image):
+    context = base64.b64encode(image.read()).decode()
     img_requests = [{
         'image': {
             'content': context
@@ -26,29 +27,48 @@ def get_str(image):
     response = requests.post(os.environ.get("GOOGLE_ENDPOINT"),
                              data=json.dumps({
                                  'requests': img_requests
-                             }).encode(),   # .encode()？
+                             }).encode(),   # .encode()
                              params={'key': os.environ.get("GOOGLE_API_KEY")},
                              headers={'Content-Type': 'application/json'}).json()
-    # print(response['responses'][0]['fullTextAnnotation']['text'])
+    print(response['responses'][0]['fullTextAnnotation']['text'])
     return response
 
 
 def extract_jan(response_text):
-    digested_text = response_text.replace('\"', '').replace(' ', '').replace('\n', '')
-    print(digested_text)
-    result = re.findall('\d{13}', digested_text)
-    print(result)
-    return result
+    # digested_text = response_text.replace('\"', '').replace(' ', '').replace('\n', '')
+    # jans = re.findall('\d{13}', digested_text)
+    jans = re.findall('\d{13}', response_text)
+    print(jans)
+    return jans
+
+
+def request_to_rakuten(jans):
+    item_list = []
+    for jan in jans:
+        payload = {'applicationId': os.environ.get("RAKUTEN_APPLICATION_ID"), 'keyword': jan, 'hits': 1}
+        response = requests.get(os.environ.get("RAKUTEN_ENDPOINT"), params=payload).json()
+        if len(response['Items']) > 0:
+            item = response['Items'][0]['Item']
+            print(item['itemName'])
+            item_list.append(item)
+            time.sleep(1)
+    print(item_list)
+    return item_list
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        response = get_str(request.files['image'])
+        response = request_to_gcp(request.files['image'])
         # print(response['responses'][0]['fullTextAnnotation']['text'])
         # response_text = process_str(response['responses'][0]['fullTextAnnotation']['text'])
-        response_text = extract_jan(response['responses'][0]['textAnnotations'][0]['description'])
-        return render_template('capture.html', response_text=response_text)
+        jans = extract_jan(response['responses'][0]['textAnnotations'][0]['description'])
+        item_list = request_to_rakuten(jans)
+        if item_list:
+            message = '解析できました！'
+        else:
+            message = '解析できませんでした...'
+        return render_template('capture.html', item_list=item_list, message=message)
     else:
         return render_template('capture.html')
 
